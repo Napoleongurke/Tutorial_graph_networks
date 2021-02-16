ade20913c67a40eb08c20cf356656e7ef3455bde
JGlombitza <jonas.glombitza@rwth-aachen.de>
2021-02-16 12:41:09 +0100

    Update utils.py
    Change to directed graph plotting

--- utils.py
+++ utils.py
@@ -17,6 +17,7 @@ def get_edge_graph_in_model(input_data, model, sample_idx=0):
     coord_mask = [np.sum(np.linalg.norm(inp_d[sample_idx], axis=-1)) == 500 for inp_d in input_data]
     assert True in coord_mask, "For plotting the spherical graph of the cosmic ray on at least one input has to have 3 dimensions XYZ"
     fig, axes = plt.subplots(ncols=len(layer_names), figsize=(5 * len(layer_names), 6))
+    
     for i, lay_name in enumerate(layer_names):
         points_in, feats_in = model.get_input_at(0)
         coordinates = model.get_layer(lay_name).get_input_at(0)
@@ -28,20 +29,21 @@ def get_edge_graph_in_model(input_data, model, sample_idx=0):
             layer_points = functor(sample_input)
         layer_points = np.squeeze(layer_points)
         adj = kneighbors_graph(layer_points, model.get_layer(lay_name).K)
-        g = nx.from_scipy_sparse_matrix(adj)
+        g = nx.DiGraph(adj)
+        
         for c, s in zip(coord_mask, sample_input):
             if c == True:  # no 'is' here!
                 pos = s
                 break
-        axes[i].set_title("XY-Projection of Graph formed in %s" % lay_name)
 
-        nx.draw(
-            g,
-            cmap=plt.get_cmap('viridis'),
-            pos=pos.squeeze()[:, :-1],
-            node_size=10,
-            width=0.3,
-            ax=axes[i])
+        axes[i].set_title("XY-Projection of Graph formed in %s" % lay_name)
+        nx.draw(g,
+                cmap=plt.get_cmap('viridis'),
+                pos=pos.squeeze()[:, :-1],
+                node_size=10,
+                width=0.5,
+                arrowsize=5,
+                ax=axes[i])
         axes[i].axis('equal')
     fig.tight_layout()
     return fig
@@ -59,6 +61,7 @@ def plot_eigenvectors(A):
     nrows = 4
     fig, axes = plt.subplots(nrows= nrows, ncols=int(np.ceil(len(Vs)/nrows)), figsize=(16,9))
     axes = axes.flatten()
+
     for i, eigen in enumerate(Vs):
         axes[i].imshow(eigen.reshape(28,28))
         axes[i].axis('equal')
@@ -102,6 +105,7 @@ def draw_signal_contribution(model, test_input_data, test_id=0, path="./"):
     F = functor(test_sample_points).squeeze()  # output of last conv layer
     W, b = model.get_layer("classification").get_weights()  # weights of final dense layer
     score = F[..., np.newaxis] * W  # leave out bias for better visualization
+
     for c, s in zip(coord_mask, test_sample_points):
         if c == True:  # no 'is' here!
             pos = s
